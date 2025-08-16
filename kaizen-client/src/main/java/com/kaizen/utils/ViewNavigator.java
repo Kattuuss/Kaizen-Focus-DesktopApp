ab2349eadfaa084d0e3bdc638df4b7e08a986b3b
Kattus <alexvalverdetorres2006@gmail.com>
2025-08-16 19:29:16 +0200

    🟣 added the base of the animated timer

---
+++ kaizen-client/src/main/java/com/kaizen/utils/ViewNavigator.java
@@ -0,0 +1,10 @@
+package com.kaizen.utils;
+
+/**
+ * ViewNavigator is a utility class responsible for managing the navigation
+ * between different scenes within the same primary stage (window) of a JavaFX application.
+ * It provides methods to set the main stage and switch between different views (scenes)
+ */
+public class ViewNavigator {
+
+}
--- kaizen-client/src/main/java/module-info.java
+++ kaizen-client/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module kaizen.client {
     requires javafx.graphics;
+    requires java.xml;
+    requires java.desktop;
 
     exports com.kaizen;
 }
